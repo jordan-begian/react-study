7e2afab8af120f278a49c034083a27706f0d1515
Jordan Begian <40347351+jordan-begian@users.noreply.github.com>
2023-10-23 16:04:47 -0400

    add handlePlay function to set history and player turn state

--- tic-tac-toe_tutorial/src/App.js
+++ tic-tac-toe_tutorial/src/App.js
@@ -3,11 +3,17 @@ import { useState } from "react";
 export default function Game() {
     const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
+    const currentSquares = history[history.length - 1];
+
+    function handlePlay(nextSquares) {
+        setHistory([...history, nextSquares]);
+        setXIsNext(!xIsNext)
+    }
 
     return (
         <div className="game">
             <div className="game-board">
-                <Board />
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
             </div>
             <div className="game-info">
                 <ol>{/*TODO*/}</ol>
@@ -16,10 +22,7 @@ export default function Game() {
     );
 }
 
-function Board() {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
     function handleClick(index) {
         if (squares[index] || calculateWinner(squares)) {
             return;
@@ -32,8 +35,7 @@ function Board() {
         } else {
             nextSquares[index] = "O";
         }
-        setSquares(nextSquares);
-        setXIsNext(!xIsNext);
+        onPlay(nextSquares);
     }
 
     const winner = calculateWinner(squares);
