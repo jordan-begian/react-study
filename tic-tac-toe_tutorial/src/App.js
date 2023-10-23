bd78e6835362d999471c5c5c63cc03a598805d85
Jordan Begian <40347351+jordan-begian@users.noreply.github.com>
2023-10-22 20:19:43 -0400

    refactor so that square component is below the main component

--- tic-tac-toe_tutorial/src/App.js
+++ tic-tac-toe_tutorial/src/App.js
@@ -1,16 +1,5 @@
 import { useState } from "react";
 
-function Square({ value, onSquareClick }) {
-    return (
-        <button
-            className="square"
-            onClick={onSquareClick}
-        >
-            {value}
-        </button>
-    );
-}
-
 export default function Board() {
     const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
@@ -78,5 +67,16 @@ export default function Board() {
                 />
             </div>
         </>
+    );
+}
+
+function Square({ value, onSquareClick }) {
+    return (
+        <button
+            className="square"
+            onClick={onSquareClick}
+        >
+            {value}
+        </button>
     );
 }
